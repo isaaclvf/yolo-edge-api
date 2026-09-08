43efb2799aead70dcb08abc9decc25e62ce3e362
Isaac Farias <isaacfarias006@gmail.com>
2026-09-08 00:01:10 -0300

    chore: ruff check

---
+++ scripts/status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo '--- Status do Sistema ---'
+echo -n 'Temperatura: '
+cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1/1000 " °C"}'
+echo -n 'RAM livre: '
+free -h | grep Mem | awk '{print $4}'
+echo -n 'API status: '
+curl -sf http://localhost:8000/health | jq -r '.status'
+echo -n 'Requests totais: '
+curl -sf http://localhost:8000/metrics | jq -r '.total_requests'
+echo -n 'Latência média: '
+curl -sf http://localhost:8000/metrics | jq -r '.avg_inference_ms'
+echo ' ms'
